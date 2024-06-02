dba34e78d02407729722a20acec0a924bc63d49b
MarcusCastro0701 <marcus.castro0701@gmail.com>
2024-06-02 19:56:40 -0300

    add: node caching

--- src/services/clients-service.ts
+++ src/services/clients-service.ts
@@ -1,76 +1,108 @@
 import { prisma } from '@/config';
 import { badRequestError, notFoundError } from '@/errors';
+import NodeCache = require('node-cache');
+
+const cache = new NodeCache({ stdTTL: 30 });
 
 
-async function getAllClients(){
-    const result = await prisma.clientes.findMany();
-
-    return result
-}
-
-async function createClient(nome: string, sobrenome: string, email: string, idade: number){
-    const result = await prisma.clientes.create({
-        data: {
-            nome,
-            sobrenome, 
-            email,
-            idade
-        }
-    });
-
-    if(!result){
-        throw badRequestError('CreationError');
+export async function getAllClients() {
+    const cachedClients = cache.get('allClients');
+  
+    if (cachedClients) {
+      console.log('Servindo do cache');
+      return cachedClients;
     }
-
-    return result
-}
-
-async function updateClient(id: number, nome: string, sobrenome: string, email: string, idade: number){
-
-    const clienteExistente = await prisma.clientes.findUnique({
-        where: { id },
-      });
-
-      const dadosAtualizados = {
+  
+    const result = await prisma.clientes.findMany();
+    console.log('Servindo do banco de dados');
+    cache.set('allClients', result);
+  
+    return result;
+  }
+  
+  export async function createClient(nome: string, sobrenome: string, email: string, idade: number) {
+    const result = await prisma.clientes.create({
+      data: {
         nome,
         sobrenome,
         email,
         idade
       }
-
-      const dadosMesclados = { ...clienteExistente, ...dadosAtualizados };
-
-      
-    return await prisma.clientes.update({
-        where: { id },
-        data: dadosMesclados,
-      });
-}
-
-async function deleteClient(id: number){
+    });
+  
+    if (!result) {
+      throw badRequestError('CreationError');
+    }
+  
+    cache.del('allClients');
+    return result;
+  }
+  
+  export async function updateClient(id: number, nome: string, sobrenome: string, email: string, idade: number) {
+    const clienteExistente = await prisma.clientes.findUnique({
+      where: { id },
+    });
+  
+    if (!clienteExistente) {
+      throw notFoundError();
+    }
+  
+    const dadosAtualizados = {
+      nome,
+      sobrenome,
+      email,
+      idade
+    };
+  
+    const dadosMesclados = { ...clienteExistente, ...dadosAtualizados };
+  
+    const updatedClient = await prisma.clientes.update({
+      where: { id },
+      data: dadosMesclados,
+    });
+  
+    cache.del('allClients');
+    cache.del(`client_${id}`); 
+  
+    return updatedClient;
+  }
+  
+  export async function deleteClient(id: number) {
     const result = await prisma.clientes.delete({
-        where: {id}
+      where: { id }
     });
-
-    if(!result){
-        throw notFoundError()
+  
+    if (!result) {
+      throw notFoundError();
     }
-
-    return result
-}
-
-async function findClientById(id: number){
-
+  
+    cache.del('allClients');
+    cache.del(`client_${id}`);
+  
+    return result;
+  }
+  
+  export async function findClientById(id: number) {
+    const cachedClient = cache.get(`client_${id}`);
+  
+    if (cachedClient) {
+      console.log('Servindo do cache');
+      return cachedClient;
+    }
+  
     const find = await prisma.clientes.findUnique({
-        where: {id}
+      where: { id }
     });
-
-    if(!find){
-        throw notFoundError();
+  
+    if (!find) {
+      throw notFoundError();
     }
-
-    return find
-}
+  
+    console.log('Servindo do banco de dados');
+    cache.set(`client_${id}`, find);
+  
+    return find;
+  }
 
 const clientsService = {
     getAllClients,
